44b41760a62a7722d90b01f1c86c5ff3f97d5abf
Matias Borghi <borghi.matias@gmail.com>
2019-05-10 11:34:32 -0300

    :fire: Minimal Webpack Configuration running

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    module: {
+        rules: [
+            {   
+                // Load babel loader. To understand js in all browsers
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                // Find all the html and minimize them
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+             {   
+                // Load file loader. To understand all image files
+                test: /\.(png|svg|jpg|gif)$/,
+                use: {
+                    loader: "file-loader"
+                }
+            },
+            {
+                // Load packages to scss files
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // The first plugin we load is for webpack to copy to dist the html file
+        new HtmlWebPackPlugin({
+            template:"./src/index.html",
+            filename: "./index.html"
+        }),
+        // This plugin will make sure css and sass files are included in the production build
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFileName: "[id].css"
+        })
+    ]
+}
+
